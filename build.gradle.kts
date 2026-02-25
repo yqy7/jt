82fefd8a1436ac0a6fad0028400dfd3bc8775968
yqy7 <qiyun.yqy@gmail.com>
2026-02-25 08:59:51 +0800

    add maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 group = "io.github.yqy7"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -17,4 +18,37 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(group.toString(), name, version.toString())
+
+    pom {
+        name.set("jt")
+        description.set("Java tools")
+        inceptionYear.set("2026")
+        url.set("https://github.com/yqy7/jt")
+        licenses {
+            license {
+                name.set("The MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("yqy7")
+                name.set("yqy7")
+                url.set("https://github.com/yqy7/")
+            }
+        }
+        scm {
+            url.set("https://github.com/yqy7/jt")
+            connection.set("scm:git:git://github.com/yqy7/jt.git")
+            developerConnection.set("scm:git:ssh://git@github.com/yqy7/jt.git")
+        }
+    }
 }
